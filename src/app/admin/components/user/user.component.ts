b65dbdc1ab0eff24fa4b8722fc8e475b4d9df069
ngoclias <ngoc.leanh1999@gmail.com>
2022-03-27 23:44:11 +0700

    [NgocLA4] Nothing

--- src/app/admin/components/user/user.component.ts
+++ src/app/admin/components/user/user.component.ts
@@ -2,10 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
-import { userCreate} from 'src/app/shared/models/user.model';
+import { userCreate } from 'src/app/shared/models/user.model';
 import { UserService } from 'src/app/shared/services/user/user.service';
 import Swal from 'sweetalert2';
-
 
 @Component({
   selector: 'app-user',
@@ -13,42 +12,36 @@ import Swal from 'sweetalert2';
   styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
-
-   user: any;
-   isLoading = false;
-   totalRecords:any
-   uploadedFiles: any[] = [];
-   formsearch: any;
-   formdata: any;
-   doneSetupForm: any;  
-   showUpdateModal:any;
+  user: any;
+  isLoading = false;
+  totalRecords: any;
+  uploadedFiles: any[] = [];
+  formsearch: any;
+  formdata: any;
+  doneSetupForm: any;
+  showUpdateModal: any;
   public unsubscribe = new Subject();
-  
-  public isCreate:any;
+
+  public isCreate: any;
   submitted = false;
 
   users: any[] = [];
   exportUsers: userCreate[] = [];
 
-  constructor(private fb: FormBuilder, private userService: UserService) { }
+  constructor(private fb: FormBuilder, private userService: UserService) {}
 
   p: number = 1;
 
   ngOnInit(): void {
-    // for(let i = 0; i <= 10; i++) {
-    //   this.users.push({userName: `user${i}`,
-    //     email: `abc${i}@gmail.com`, password: `000${i}`, role: `user${i}, admin${i}`})
-    // }
     this.isLoading = true;
     this.getAllUser();
   }
 
   getAllUser() {
-    this.userService.queryUser().subscribe(data => {
+    this.userService.queryUser().subscribe((data) => {
       this.isLoading = false;
       this.users = data.data.result;
-      console.log(this.users);
-    })
+    });
   }
 
   onSubmit(value: any) {
@@ -63,106 +56,114 @@ export class UserComponent implements OnInit {
         password: value.password,
         role: value.role,
       };
-      this.userService.createUser(tmp).pipe(takeUntil(this.unsubscribe)).subscribe(res => {
-        const Toast = Swal.mixin({
-          toast: true,
-          position: 'center',
-          showConfirmButton: false,
-          timer: 2500,
-          timerProgressBar: true,
-          didOpen: (toast) => {
-            toast.addEventListener('mouseenter', Swal.stopTimer);
-            toast.addEventListener('mouseleave', Swal.resumeTimer);
-          },
-        });
+      this.userService
+        .createUser(tmp)
+        .pipe(takeUntil(this.unsubscribe))
+        .subscribe(
+          (res) => {
+            const Toast = Swal.mixin({
+              toast: true,
+              position: 'center',
+              showConfirmButton: false,
+              timer: 2500,
+              timerProgressBar: true,
+              didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer);
+                toast.addEventListener('mouseleave', Swal.resumeTimer);
+              },
+            });
 
-        Toast.fire({
-          icon: 'success',
-          title: 'Create User success!!!',
-          
-        });
-        this.closeModal();
-        this.getAllUser();
-      },
-      (err) => {
-        const Toast = Swal.mixin({
-          toast: true,
-          position: 'center',
-          showConfirmButton: false,
-          timer: 2500,
-          timerProgressBar: true,
-          didOpen: (toast) => {
-            toast.addEventListener('mouseenter', Swal.stopTimer);
-            toast.addEventListener('mouseleave', Swal.resumeTimer);
+            Toast.fire({
+              icon: 'success',
+              title: 'Create User success!!!',
+            });
+            this.closeModal();
+            this.getAllUser();
           },
-        });
+          (err) => {
+            const Toast = Swal.mixin({
+              toast: true,
+              position: 'center',
+              showConfirmButton: false,
+              timer: 2500,
+              timerProgressBar: true,
+              didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer);
+                toast.addEventListener('mouseleave', Swal.resumeTimer);
+              },
+            });
 
-        Toast.fire({
-          icon: 'error',
-          title: 'Create fail!!!',
-        });
-        this.closeModal();
-        this.getAllUser();
-      },
-      )
+            Toast.fire({
+              icon: 'error',
+              title: 'Create fail!!!',
+            });
+            this.closeModal();
+            this.getAllUser();
+          }
+        );
     } else {
       let tmp = {
         username: value.username,
         role: value.role,
       };
-      this.userService.updateUserById(tmp, this.user.id).pipe(takeUntil(this.unsubscribe)).subscribe(res => {
-        const Toast = Swal.mixin({
-          toast: true,
-          position: 'center',
-          showConfirmButton: false,
-          timer: 2500,
-          timerProgressBar: true,
-          didOpen: (toast) => {
-            toast.addEventListener('mouseenter', Swal.stopTimer);
-            toast.addEventListener('mouseleave', Swal.resumeTimer);
+      this.userService
+        .updateUserById(tmp, this.user.id)
+        .pipe(takeUntil(this.unsubscribe))
+        .subscribe(
+          (res) => {
+            const Toast = Swal.mixin({
+              toast: true,
+              position: 'center',
+              showConfirmButton: false,
+              timer: 2500,
+              timerProgressBar: true,
+              didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer);
+                toast.addEventListener('mouseleave', Swal.resumeTimer);
+              },
+            });
+
+            Toast.fire({
+              icon: 'success',
+              title: 'Update User success!!!',
+            });
+            this.closeModal();
+            this.getAllUser();
           },
-        });
+          (err) => {
+            const Toast = Swal.mixin({
+              toast: true,
+              position: 'center',
+              showConfirmButton: false,
+              timer: 2500,
+              timerProgressBar: true,
+              didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer);
+                toast.addEventListener('mouseleave', Swal.resumeTimer);
+              },
+            });
 
-        Toast.fire({
-          icon: 'success',
-          title: 'Update User success!!!',
-          
-        });
-        this.closeModal();
-        this.getAllUser();
-      },
-      (err) => {
-        const Toast = Swal.mixin({
-          toast: true,
-          position: 'center',
-          showConfirmButton: false,
-          timer: 2500,
-          timerProgressBar: true,
-          didOpen: (toast) => {
-            toast.addEventListener('mouseenter', Swal.stopTimer);
-            toast.addEventListener('mouseleave', Swal.resumeTimer);
-          },
-        });
-
-        Toast.fire({
-          icon: 'error',
-          title: 'Update fail!!!',
-        });
-        this.closeModal();
-        this.getAllUser();
-
-      },)
+            Toast.fire({
+              icon: 'error',
+              title: 'Update fail!!!',
+            });
+            this.closeModal();
+            this.getAllUser();
+          }
+        );
     }
   }
   Reset() {
     this.user = null;
-    this.formdata = this.fb.group({
-      'username': ['', Validators.required],
-      'email': ['', Validators.required],
-      'password': [''],
-      'role': ['', Validators.required],
-    }, {
-    }); 
+    this.formdata = this.fb.group(
+      {
+        username: ['', Validators.required],
+        email: ['', Validators.required],
+        password: [''],
+        role: ['', Validators.required],
+      },
+      {}
+    );
   }
 
   get f() {
@@ -194,7 +195,6 @@ export class UserComponent implements OnInit {
           icon: 'success',
           showCancelButton: false,
           confirmButtonColor: '#273043',
-          
         });
       }
     });
@@ -207,14 +207,15 @@ export class UserComponent implements OnInit {
     this.user = null;
     setTimeout(() => {
       (<any>$('#createUserModal')).modal('toggle');
-      this.formdata = this.fb.group({
-        'username': ['', Validators.required],
-        'email': ['', Validators.required],
-        'password': [''],
-        'role': ['', Validators.required],
-      }, {
-       
-      });
+      this.formdata = this.fb.group(
+        {
+          username: ['', Validators.required],
+          email: ['', Validators.required],
+          password: [''],
+          role: ['', Validators.required],
+        },
+        {}
+      );
       this.doneSetupForm = true;
     });
   }
@@ -223,25 +224,33 @@ export class UserComponent implements OnInit {
     this.doneSetupForm = false;
     this.showUpdateModal = true;
     this.isCreate = false;
-    this.userService.getUserById(id).pipe(takeUntil(this.unsubscribe)).subscribe(res => {
-      let response: any = res;
-      this.user = response.data;
-      console.log(this.user);
-       setTimeout(() => {
-        (<any>$('#createUserModal')).modal('toggle');
-     
-      this.formdata = this.fb.group({
-        'username': [this.user.username, Validators.required],
-        'email': [this.user.email, Validators.required],
-        'password': [this.user.password],
-        'role': [this.user.role, Validators.required],
-          }, {
-          }); 
-          this.doneSetupForm = true;
-        });
-      }, err => {
-        console.error(err.error);
-      })
+    this.userService
+      .getUserById(id)
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(
+        (res) => {
+          let response: any = res;
+          this.user = response.data;
+          console.log(this.user);
+          setTimeout(() => {
+            (<any>$('#createUserModal')).modal('toggle');
+
+            this.formdata = this.fb.group(
+              {
+                username: [this.user.username, Validators.required],
+                email: [this.user.email, Validators.required],
+                password: [this.user.password],
+                role: [this.user.role, Validators.required],
+              },
+              {}
+            );
+            this.doneSetupForm = true;
+          });
+        },
+        (err) => {
+          console.error(err.error);
+        }
+      );
   }
 
   closeModal() {

8e58ecf4e41f9d2b4bd58ae0971813a784c86e0b
ngoclias <ngoc.leanh1999@gmail.com>
2022-04-14 23:51:27 +0700

    [NgocLA4] update

--- src/app/admin/components/admin/admin-aside/admin-aside.component.ts
+++ src/app/admin/components/admin/admin-aside/admin-aside.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-admin-aside',
   templateUrl: './admin-aside.component.html',
-  styleUrls: ['./admin-aside.component.css']
+  styleUrls: ['./admin-aside.component.css'],
 })
 export class AdminAsideComponent implements OnInit {
+  showItem: boolean = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }

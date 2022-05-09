f58dd0ed0758a144592ea66f645b71d2be80977d
ngoclias <ngoc.leanh1999@gmail.com>
2022-05-09 22:02:05 +0700

    add navbar admin

--- src/app/admin/components/admin/navbar/admin-navbar/admin-navbar.component.ts
+++ src/app/admin/components/admin/navbar/admin-navbar/admin-navbar.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { UserInfo } from 'src/app/shared/models/user.model';
+import { SwalAlertService } from 'src/app/shared/services/others/swal-alert.service';
+import { UserService } from 'src/app/shared/services/user/user.service';
 
 @Component({
   selector: 'app-admin-navbar',
   templateUrl: './admin-navbar.component.html',
-  styleUrls: ['./admin-navbar.component.css']
+  styleUrls: ['./admin-navbar.component.css'],
 })
 export class AdminNavbarComponent implements OnInit {
-
-  constructor() { }
+  userInfo!: UserInfo;
+  constructor(
+    private userService: UserService,
+    private swalAlert: SwalAlertService
+  ) {}
 
   ngOnInit() {
+    this.userService.getUserProfile().subscribe(
+      (user) => {
+        this.userInfo = user;
+        console.log(this.userInfo);
+      },
+      (error) => {
+        const message = error.error.message;
+        this.swalAlert.textAlert('error', message);
+      }
+    );
   }
-
 }
